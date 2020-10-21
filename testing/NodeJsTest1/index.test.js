e7f44f79ded52eb2e9beb0f400aec86195684184
Prasanta <pkbtest1@outlook.com>
2020-10-21 23:56:32 +0530

    ff

--- testing/NodeJsTest1/index.test.js
+++ testing/NodeJsTest1/index.test.js
@@ -4,7 +4,7 @@ const httpFunction = require("../../NodeJsTest1/index");
 
 test("Http trigger should return known text", async() => {
     const request = {
-        query: { name: "Bill" },
+        query: { name: "Bill1" },
     };
 
     await httpFunction(context, request);
